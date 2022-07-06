83a99636d905034ea823eff5ab90f07c9406264b
ke5hv <107155191+ke5hv@users.noreply.github.com>
2022-07-06 16:01:55 -0400

    Update Finalinclass.java
    branch name

--- src/main/java/com/mycompany/finalinclass/Finalinclass.java
+++ src/main/java/com/mycompany/finalinclass/Finalinclass.java
@@ -13,6 +13,7 @@ public class Finalinclass {
 
     public static void main(String[] args) {
         System.out.println("Loyalist College ");
+        System.out.println("Branch30 ");
      
         
         
